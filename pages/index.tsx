0add5eb0ed7bc04604bf800b4f35bf5763f5ef44
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-05-26 20:17:55 +0200

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      Home
+    </div>
+  );
+};
+
+export default Home;
