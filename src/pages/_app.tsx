08329b9b41ae3e9e01931402744a8a9a421bf158
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-05-27 02:51:09 +0200

    feat(_app): use `AuthContext`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from "next/app"
 import Layout from "../layouts/Layout"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { AuthProvider } from "../contexts/AuthContext"
 
 const theme = createTheme()
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
     </ThemeProvider>
   )
 }
