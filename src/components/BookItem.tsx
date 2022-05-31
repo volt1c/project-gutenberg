804a33632a62670f0fd6a59b20b431383b1c024e
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-05-31 20:50:28 +0200

    feat: implement properly read, view buttons

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -7,7 +7,10 @@ import {
   Button,
 } from "@mui/material"
 import Link from "next/link"
-import { IBook } from "../types/apiTypes"
+import { useState } from "react"
+import { IBook } from "../types/api"
+import { findWebsiteResource } from "../utils/bookResources"
+import ReadDialog from "./ReadDialog"
 
 interface BookItemProps {
   book: IBook
@@ -16,43 +19,61 @@ interface BookItemProps {
 function BookItem({
   book: { id, title, description, resources, agents },
 }: BookItemProps) {
+  const [open, setOpen] = useState(false)
+  const [src, setSrc] = useState<string>()
+
   return (
-    <Card
-      sx={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <CardMedia
+    <>
+      <Card
         sx={{
-          objectFit: "contain",
-          maxHeight: "640px",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
         }}
-        component="img"
-        image={
-          resources
-            .filter((r) => r.type.includes("image/jpeg"))
-            .find((r) => r.uri.includes("medium"))?.uri ?? "/bookcover.jpg"
-        }
-        alt="random"
+      >
+        <CardMedia
+          sx={{
+            objectFit: "contain",
+            maxHeight: "640px",
+          }}
+          component="img"
+          image={
+            resources
+              .filter((r) => r.type.includes("image/jpeg"))
+              .find((r) => r.uri.includes("medium"))?.uri ?? "/bookcover.jpg"
+          }
+          alt="random"
+        />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography>{description}</Typography>
+          <Typography variant="caption">
+            {agents.map((a) => a.person).join(", ")}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link href={`/book/${id}`}>
+            <Button size="small">View</Button>
+          </Link>
+          <Button
+            size="small"
+            onClick={() => {
+              if (!src) setSrc(findWebsiteResource(resources ?? [])?.uri ?? "")
+              setOpen(true)
+            }}
+          >
+            Read
+          </Button>
+        </CardActions>
+      </Card>
+      <ReadDialog
+        open={open}
+        onClose={() => setOpen(false)}
+        bookSrc={src ?? ""}
       />
-      <CardContent sx={{ flexGrow: 1 }}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {title}
-        </Typography>
-        <Typography>{description}</Typography>
-        <Typography variant="caption">
-          {agents.map((a) => a.person).join(", ")}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View</Button>
-        <Link href={`/book/${id}`}>
-          <Button size="small">Read</Button>
-        </Link>
-      </CardActions>
-    </Card>
+    </>
   )
 }
 
