58ee24c8feb25397f42b40b2b5a220b7c12576c7
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-06-04 23:08:46 +0200

    refactor(BookItem): easier to understand

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -39,6 +39,44 @@ function BookItem({ book }: BookItemProps) {
       isFavorite(auth.user.uid, book.id).then((b) => setIsFav(b))
   }, [auth.isReady, auth.user, book])
 
+  if (!book)
+    return (
+      <>
+        <Card
+          sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Skeleton height="400px" animation="wave" variant="rectangular" />
+
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography gutterBottom variant="h5" component="h2" height="64px">
+              <Skeleton />
+              <Skeleton />
+            </Typography>
+            <Typography height="72px">
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </Typography>
+            <Typography variant="caption" component="p">
+              <Skeleton />
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <>
+              <Skeleton width="54px" height="23px" />
+              <Skeleton width="54px" height="23px" />
+              <Box sx={{ flexGrow: 1 }} />
+              <Skeleton variant="circular" width="24px" height="24px" />
+            </>
+          </CardActions>
+        </Card>
+      </>
+    )
+
   const short = (str: string, newLen: number, end = "...") =>
     str.length > newLen ? str.slice(0, newLen - 3) + end : str
 
@@ -51,87 +89,62 @@ function BookItem({ book }: BookItemProps) {
           flexDirection: "column",
         }}
       >
-        {book ? (
-          <CardMedia
-            sx={{
-              objectFit: "cover",
-              height: "400px",
-            }}
-            component="img"
-            image={
-              book?.resources
-                .filter((r) => r.type.includes("image/jpeg"))
-                .find((r) => r.uri.includes("medium"))?.uri ?? "/bookcover.jpg"
-            }
-            alt="random"
-          />
-        ) : (
-          <Skeleton height="400px" animation="wave" variant="rectangular" />
-        )}
+        <CardMedia
+          sx={{
+            objectFit: "cover",
+            height: "400px",
+          }}
+          component="img"
+          image={
+            book?.resources
+              .filter((r) => r.type.includes("image/jpeg"))
+              .find((r) => r.uri.includes("medium"))?.uri ?? "/bookcover.jpg"
+          }
+          alt="random"
+        />
         <CardContent sx={{ flexGrow: 1 }}>
           <Typography gutterBottom variant="h5" component="h2" height="64px">
-            {book ? short(book?.title, 30) : <Skeleton />}
+            {short(book?.title, 30)}
           </Typography>
           <Typography height="72px">
-            {book ? (
-              short(book?.description ?? "No description", 80)
-            ) : (
-              <Skeleton />
-            )}
+            {short(book?.description ?? "No description", 80)}
           </Typography>
           <Typography variant="caption" component="p">
-            {book ? (
-              short(book?.agents.map((a) => a.person).join(", "), 40)
-            ) : (
-              <Skeleton />
-            )}
+            {short(book?.agents.map((a) => a.person).join(", "), 40)}
           </Typography>
         </CardContent>
         <CardActions>
-          {book ? (
-            <>
-              <Link href={`/book/${book?.id}`}>
-                <Button size="small">View</Button>
-              </Link>
-              <Button
-                size="small"
-                onClick={() => {
-                  if (!src)
-                    setSrc(
-                      findWebsiteResource(book?.resources ?? [])?.uri ?? ""
-                    )
-                  setOpen(true)
-                }}
-              >
-                Read
-              </Button>
-              <Box sx={{ flexGrow: 1 }} />
-              <IconButton
-                onClick={() => {
-                  if (auth.user?.uid && book) {
-                    ;(async () => {
-                      if (isFav)
-                        await deleteFavoriteBook(auth.user?.uid ?? "", book?.id)
-                      else await addFavoriteBook(auth.user?.uid ?? "", book?.id)
-                      setIsFav(!isFav)
-                    })()
+          <Link href={`/book/${book?.id}`}>
+            <Button size="small">View</Button>
+          </Link>
+          <Button
+            size="small"
+            onClick={() => {
+              if (!src)
+                setSrc(findWebsiteResource(book?.resources ?? [])?.uri ?? "")
+              setOpen(true)
+            }}
+          >
+            Read
+          </Button>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton
+            onClick={() => {
+              if (auth.user?.uid && book) {
+                ;(async () => {
+                  if (isFav)
+                    await deleteFavoriteBook(auth.user?.uid ?? "", book?.id)
+                  else await addFavoriteBook(auth.user?.uid ?? "", book?.id)
+                  setIsFav(!isFav)
+                })()
 
-                    return
-                  }
-                  Router.push("/signin")
-                }}
-              >
-                <Star sx={{ fill: isFav ? "yellow" : "inherit" }} />
-              </IconButton>
-            </>
-          ) : (
-            <>
-              <Skeleton width="54px" height="23px" />
-              <Skeleton width="54px" height="23px" />
-              <Box sx={{ flexGrow: 1 }} />
-              <Skeleton variant="circular" width="24px" height="24px" />
-            </>
-          )}
+                return
+              }
+              Router.push("/signin")
+            }}
+          >
+            <Star sx={{ fill: isFav ? "yellow" : "inherit" }} />
+          </IconButton>
         </CardActions>
       </Card>
       <ReadDialog
