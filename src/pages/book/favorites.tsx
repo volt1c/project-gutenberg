c09bf85cb66c48ba982dda8eeee0b914b7e93a9b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-06-04 21:57:47 +0200

    feat(favorites): make search work properly

--- src/pages/book/favorites.tsx
+++ src/pages/book/favorites.tsx
@@ -1,21 +1,24 @@
-import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
-import { Paper, InputBase, IconButton, Stack } from "@mui/material"
+import { Paper, InputBase, IconButton } from "@mui/material"
 import SearchIcon from "@mui/icons-material/Search"
-import { useEffect, useState } from "react"
-import FiltersDialog from "../../components/FiltersDialog"
-import Router from "next/router"
+import { FormEvent, useEffect, useRef, useState } from "react"
+import { useRouter } from "next/router"
 import { useAuthContext } from "../../contexts/AuthContext"
 import { getFavoritesByUserUid } from "../../firebase/firestore"
 import { IBook } from "../../types/api"
 import { fetchBooksByIds } from "../../utils/fetchApi"
 import BookScroll from "../../components/BookScroll"
+import Head from "next/head"
 
 function BooksFavoritesPage() {
   const [open, setOpen] = useState(false)
   const [count, setCount] = useState(-1)
   const [visible, setVisible] = useState(false)
   const [books, setBooks] = useState<IBook[]>([])
+
+  const searchInput = useRef<HTMLInputElement>()
+
+  const router = useRouter()
 
   const auth = useAuthContext()
   const { user, isReady } = auth
@@ -24,25 +27,35 @@ function BooksFavoritesPage() {
 
   useEffect(() => {
     if (!isReady) return
-    if (!user) Router.push("/signin")
+    if (!user) router.push("/signin")
     if (user) {
       setVisible(true)
 
       getFavoritesByUserUid(user?.uid).then((favorites) => {
-        setCount(favorites.length)
-
-        fetchBooksByIds(favorites.map((f) => f.book_id)).then((b) => {
+        fetchBooksByIds(
+          favorites.map((f) => f.book_id),
+          router.query.search
+            ? (b) =>
+                b.title
+                  .toLowerCase()
+                  .includes(router.query.search?.toString().toLowerCase() ?? "")
+            : undefined
+        ).then((b) => {
+          setCount(favorites.length)
           setBooks(b)
         })
       })
     }
-  }, [isReady, user])
+  }, [isReady, user, router.query])
 
   return (
-    <main>
-      {visible && (
-        <Container sx={{ py: 4 }} maxWidth="xl">
-          <Stack direction="row" spacing={{ sm: 2 }} sx={{ py: 2 }}>
+    <>
+      <Head>
+        <title>Project Gutenberg | {router.query.search ?? "Favorites"}</title>
+      </Head>
+      <main>
+        {visible && (
+          <Container sx={{ py: 4 }} maxWidth="xl">
             <Paper
               component="form"
               sx={{
@@ -50,35 +63,42 @@ function BooksFavoritesPage() {
                 display: "flex",
                 alignItems: "center",
                 flexGrow: 1,
+                mb: 2,
+              }}
+              onSubmit={(event: FormEvent<HTMLFormElement>) => {
+                event.preventDefault()
+                router.push(
+                  {
+                    pathname: "/book/favorites",
+                    query: { search: searchInput.current?.value ?? "" },
+                  },
+                  undefined,
+                  { shallow: true }
+                )
               }}
             >
               <InputBase
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="Search for books..."
-                inputProps={{ "aria-label": "search for books" }}
+                inputProps={{
+                  "aria-label": "search for books",
+                  ref: searchInput,
+                }}
               />
               <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
                 <SearchIcon />
               </IconButton>
             </Paper>
-            <Button variant="contained" onClick={() => setOpen(true)}>
-              Filters
-            </Button>
-          </Stack>
-          <BookScroll
-            count={count}
-            next={() => {}}
-            hasMore={false}
-            books={books}
-          />
-        </Container>
-      )}
-      <FiltersDialog
-        open={open}
-        onClose={() => setOpen(false)}
-        onApply={(f) => console.log(f)}
-      />
-    </main>
+            <BookScroll
+              count={count}
+              next={() => {}}
+              hasMore={false}
+              books={books}
+            />
+          </Container>
+        )}
+      </main>
+    </>
   )
 }
 
